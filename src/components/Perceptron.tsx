8f924cb3ba58c91922648892dab2f4f267d46410
Eugene Lepeico <eugenelepeico@gmail.com>
2021-03-19 17:40:27 +0200

    lab 2 done

--- src/components/Perceptron.tsx
+++ src/components/Perceptron.tsx
@@ -1,18 +1,56 @@
 import React, {useState} from "react";
+import PerceptronClass from "../lib/perceptron";
 
 function Perceptron() {
+    const [learningRate, setLearningRate] = useState<number>(0.001)
+    const [deadline, setDeadline] = useState<number>(100)
+    const [accuracy, setAccuracy] = useState<number>()
+    const [perceptron, setPerceptron] = useState<PerceptronClass>()
+
     return (
         <div className="FermatFactors">
             <header className="App__Header">
-                Perceptron
+                {accuracy ? `Accuracy: ${accuracy}` : "Perceptron"}
             </header>
-            <input
-                type={"number"}
-                inputMode={"numeric"}
-                style={{borderColor: "#157F1F"}}
-            />
-            <button onClick={() => {
-            }} style={{backgroundColor: "#157F1F"}}>Calculate
+            <select
+                value={learningRate}
+                onChange={({target}) =>
+                    setLearningRate(Number.parseFloat(target.value))
+                }
+                placeholder={"Learning Rate"}
+                style={{outlineColor: "#157F1F"}}
+            >
+                <option value="0.001">0,001</option>
+                <option value="0.01">0,01</option>
+                <option value="0.05">0,05</option>
+                <option value="0.1">0,1</option>
+                <option value="0.2">0,2</option>
+                <option value="0.3">0,3</option>
+            </select>
+            <select
+                value={deadline}
+                onChange={({target}) =>
+                    setDeadline(Number.parseInt(target.value))
+                }
+                placeholder={"Deadline"}
+                style={{outlineColor: "#157F1F"}}
+            >
+                <option value="100">100</option>
+                <option value="200">200</option>
+                <option value="500">500</option>
+                <option value="1000">1000</option>
+            </select>
+            <button
+                onClick={() => {
+                    const p = perceptron || new PerceptronClass({threshold: 4, learningRate})
+
+                    setAccuracy(p.learn([[0, 6], [3, 3], [1, 5], [2, 4]], deadline))
+
+                    setPerceptron(p)
+                }}
+                style={{backgroundColor: "#157F1F"}}
+            >
+                Learn
             </button>
         </div>
     );
--- src/lib/perceptron.ts
+++ src/lib/perceptron.ts
@@ -1,10 +1,3 @@
-# RTS labs
-
-## <b>Lab 2</b> <i>"Perceptron"</i>
-
-### Code
-
-```typescript
 type Input = number[]
 
 const activations = {
@@ -84,55 +77,5 @@ class Perceptron {
         return this.threshold - y
     }
 }
-```
 
-### Screenshots
-
-![screenshot1](./examples/perceptron/screenshot1.jpeg)
-
-![screenshot2](./examples/perceptron/screenshot2.jpeg)
-
-![screenshot3](./examples/perceptron/screenshot3.jpeg)
-
-<details>
-  <summary><b>Lab 1</b> <i>"Fermat's Factors"</i></summary>
-
-### Code
-
-```typescript
-function fermatFactors(n: number): number[] {
-    if (n <= 0)
-        return [n]
-
-    if (!(n % 2))
-        return [n / 2, 2]
-
-    let a = Math.ceil(Math.sqrt(n))
-
-    if (a * a === n)
-        return [a, a]
-
-    let b = 0
-    while (1) {
-        const c = a * a - n
-        b = Math.floor(Math.sqrt(c))
-
-        if (b * b === c)
-            break
-        else
-            a += 1
-    }
-
-    return [a - b, a + b]
-}
-```
-
-### Screenshots
-
-![screenshot1](./examples/fermatFactors/screenshot1.jpeg)
-
-![screenshot2](./examples/fermatFactors/screenshot2.jpeg)
-
-![screenshot3](./examples/fermatFactors/screenshot3.jpeg)
-
-</details> 
+export default Perceptron;
